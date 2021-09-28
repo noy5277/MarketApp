9673c24a3e222b68ce25153e3b5323857122414a
Noy Zozel <noy5277@gmail.com>
2021-09-29 02:06:28 +0300

    changes

--- app/src/main/java/com/example/market_app/Cart.java
+++ app/src/main/java/com/example/market_app/Cart.java
@@ -1,14 +1,59 @@
 package com.example.market_app;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class Cart extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class Cart extends AppCompatActivity  {
+
+    private LinearLayout layout;
+    private ArrayList<String> buttonName;
+    private ArrayList<Integer> costs;
+    private Button button;
+    private TextView calcSum;
+    int i=0;
+    private int sum=0;
+    public static final String INPUT_HAIRCUT="PriceMenu.INPUT_HAIRCUT";
+    public static final String INPUT_COST="PriceMenu.INPUT_COST";
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+        layout=findViewById(R.id.mainLayout);
+        calcSum=findViewById(R.id.calcSum_Txt);
+        Bundle extras=getIntent().getExtras();
+        buttonName=new ArrayList<String>();
+        costs=new ArrayList<Integer>();
+        if(extras!=null)
+        {
+            buttonName=extras.getStringArrayList(INPUT_HAIRCUT);
+            costs=extras.getIntegerArrayList(INPUT_COST);
+        }
+        for(Integer cost:costs)
+        {
+            sum=sum+cost;
+        }
+        for(String hairCut:buttonName)
+        {
+            Button button=new Button(this);
+            button.setId(i+1);
+            button.setText(hairCut);
+            button.setTag(i);
+            button.setBackgroundColor(R.color.dark_red);
+            layout.addView(button);
+        }
+        calcSum.setText(Integer.toString(sum));
     }
+
 }
--- app/src/main/java/com/example/market_app/PriceMenu.java
+++ app/src/main/java/com/example/market_app/PriceMenu.java
@@ -2,29 +2,52 @@ package com.example.market_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class PriceMenu extends AppCompatActivity {
 
-
+    private Intent cartIntent;
+    public static final String INPUT_HAIRCUT="PriceMenu.INPUT_HAIRCUT";
+    public static final String INPUT_COST="PriceMenu.INPUT_COST";
+    private ArrayList<String> haircut;
+    private ArrayList<Integer> cost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_price_menu);
+        cartIntent=new Intent(this,Cart.class);
+        haircut=new ArrayList<String>();
+        cost=new ArrayList<Integer>();
     }
 
     public void AddChildToCart(View view) {
         Toast.makeText(this, "HairCut Added to cart", Toast.LENGTH_SHORT).show();
+        haircut.add("Child HairCut");
+        cost.add(40);
     }
 
     public void AddManToCart(View view) {
         Toast.makeText(this, "HairCut Added to cart", Toast.LENGTH_SHORT).show();
+        haircut.add("Man HairCut");
+        cost.add(60);
+
     }
 
     public void AddWomanToCart(View view) {
         Toast.makeText(this, "HairCut Added to cart", Toast.LENGTH_SHORT).show();
+        haircut.add("Woman HairCut");
+        cost.add(80);
+    }
+
+    public void OpenCartActivity(View view) {
+        cartIntent.putExtra(INPUT_HAIRCUT,haircut);
+        cartIntent.putExtra(INPUT_COST,cost);
+        startActivity(cartIntent);
     }
 }
